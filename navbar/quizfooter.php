7fd35f1aa045decaf564fa6a6107e1e84cb2fd95
Sóré Aladelusi <sore.aladelusi@yahoo.com>
2022-05-09 05:29:57 +0100

    header and footer for quiz

---
+++ navbar/quizfooter.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="footer-container">
+        <p>
+            <a href="../aboutus.php">About Us | </a>
+            &#169; Created By <a href="mailto:sore.aladelusi@yahoo.com">Oluwasore Aladelusi</a>
+            <a href="../contact.php"> | Contact Us</a>
+        </p>
+    </div>
+</body>
+</html>
---
+++ navbar/quizheader.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" type="../image/png" href="img/favicon.png">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="js/script.js" defer></script>
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="brand-title">MindOnMaths</div>
+        <a href="#" class="toggle-button">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </a>
+        <div class="navbar-links">
+            <ul>
+                <li><a href="../index.php">Home</a></li>
+                <?php
+                if (isset($_SESSION["useruid"])) {
+                    echo "<li><a href='../dashboard.php'>Dashboard</a></li>";
+                    echo "<li><a href='../settings.php'>Settings</a></li>";
+                    echo "<li><a href='../includes/logout.inc.php'>Logout</a></li>";
+                    //echo "<p>" .$_SESSION["useruid"]. "</p>";
+                } else {
+                    echo "<li><a href='../signup.php'>Sign Up</a></li>";
+                    echo "<li><a href='../login.php'>Login</a></li>";
+                }
+                ?>
+            </ul>
+        </div>
+    </nav>
+</body>
