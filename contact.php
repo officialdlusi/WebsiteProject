8658ebb436cc84605f81a24bc38b2fbfe823e616
Sóré Aladelusi <sore.aladelusi@yahoo.com>
2022-05-09 07:30:05 +0100

    Contact Form css done

--- contact.php
+++ contact.php
@@ -1,19 +1,27 @@
-<!-- <?php
+<?php
 include_once 'navbar/header.php';
-?> -->
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <body>
-        <p>SEND-EMAIL</p>
-        <form class="contact-form" action="includes/contactform.php" method="post">
-            <input type="text" name="name" placeholder="Full name">
-            <input type="text" name="mail" placeholder="Your email">
-            <input type="text" name="subject" placeholder="Subject">
-            <textarea name="message" placeholder="Message"></textarea>
-            <button type="submit" name="submit">SEND EMAIL</button>
+
+<body>
+    <div class="contactcontainer">
+        <h1>Contact Us</h1>
+        <form action="includes/contactform.php" method="post">
+            <p>Full Name</p>
+            <input type="text" name="name" placeholder="Full name" require>
+            <p>Email Address</p>
+            <input type="text" name="mail" placeholder="Your email" require>
+            <p>Subjects</p>
+            <input type="text" name="subject" placeholder="Subject" require>
+            <p>Message</p>
+            <textarea name="message" placeholder="Message"></textarea require><br>
+            <button type="submit" name="submit">Send</button>
         </form>
-    </body>
+    </div>
+</body>
+
 </html>
 
 <?php
