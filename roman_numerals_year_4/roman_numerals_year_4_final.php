91a65347fdc13b1f59eedfeae5c30d01a4eb7866
Sóré Aladelusi <sore.aladelusi@yahoo.com>
2022-05-27 12:15:05 +0100

    roman numerals page & quiz implemented

---
+++ roman_numerals_year_4/roman_numerals_year_4_final.php
@@ -0,0 +1,38 @@
+<?php
+    include_once '../includes/dbh.inc.php';
+    include '../navbar/quizheader.php';
+?>
+
+<?php
+    //Get total of questions
+    $query = "SELECT * FROM `questions` WHERE topic = 'roman_numerals_year_4'";
+
+    //Get the results
+    $results = mysqli_query($conn, $query) or die(mysqli_error($conn) . __LINE__);
+
+    $total = mysqli_num_rows($results);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="quiz-container">
+        <h2>
+            Quiz Completed!
+        </h2>
+        <p>Congratulations! You have completed the quiz!</p>
+        <p>Final Score: <?php echo ($_SESSION['score'] / $total) * 100?>%</p>
+        <button type="retake"><a href="roman_numerals_year_4.php">Retake Quiz</a></button>
+    </div>
+</body>
+
+
+</html>
---
+++ roman_numerals_year_5_quiz/roman_numerals_year_5_process.php
@@ -0,0 +1,47 @@
+<?php
+    require_once '../includes/dbh.inc.php';
+    session_start();
+?>
+
+<?php
+    //check for users score
+    if(!isset($_SESSION['score'])) {
+        $_SESSION['score'] = 0;
+    }
+
+    if($_POST) {
+        $number = $_POST['number'];
+        $selected_choice = $_POST['choice'];
+        $next = $number+1;
+        
+        //get total of questions
+        $query = "SELECT * FROM `quiz` WHERE topic = 'roman_numerals_year_5'";
+
+        //get total result
+        $result = mysqli_query($conn, $query) or die(mysqli_error($conn) . __LINE__);
+        $total = mysqli_num_rows($result);
+
+        //get correct answer
+        $query = "SELECT *  FROM `choices` WHERE questions_number = $number AND topic = 'roman_numerals_year_5' AND is_correct = 1";
+
+        //get the answer result
+        $result  = mysqli_query($conn, $query) or die(mysqli_error($conn) . __LINE__);
+
+        //get row
+        $row = mysqli_fetch_assoc($result);
+
+        //set the correct answer
+        $correct_choice = $row['id'];
+
+        if($correct_choice == $selected_choice){
+            $_SESSION['score'] ++;
+        }
+
+        //check if quiz is finished
+        if($number == $total){
+            header("location: rnfinal.php?=finished");
+            exit();
+        } else {
+            header("location: rnquiz.php?n=".$next);
+        }
+    }
